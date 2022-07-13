6d3ccff945b2b18cd11659b804ed46804ab954c4
Jpangi <106410904+Jpangi@users.noreply.github.com>
2022-07-13 15:08:16 -0700

    Update Tokyo Olympics 2021.sql

--- Tokyo Olympics 2021.sql
+++ Tokyo Olympics 2021.sql
@@ -1,5 +1,5 @@
 SELECT *
-FROM Tokoyo_Olympics.medals
+FROM Tokyo_Olympics.medals
 
 # Changing NOC column to Team
 ALTER TABLE medals RENAME COLUMN NOC TO Team;
@@ -9,7 +9,7 @@ ALTER TABLE athletes RENAME COLUMN NOC TO Team;
 # Determining which Country won the most golds
 
 SELECT Team, Gold
-FROM Tokoyo_Olympics.medals
+FROM Tokyo_Olympics.medals
 order by Gold DESC
 # Here we can see that the United States has the most gold medals with 39 
 
@@ -17,23 +17,23 @@ order by Gold DESC
 # Total Medals vs Golds
 
 SELECT Team, Gold, Total, (Gold/Total)*100 AS GoldPercentage
-FROM Tokoyo_Olympics.medals
+FROM Tokyo_Olympics.medals
 ORDER BY GoldPercentage DESC
 
 
 
 # Joining Olympics table to Medals table
 SELECT *
-FROM Tokoyo_Olympics.olympics
+FROM Tokyo_Olympics.olympics
 
 SELECT *
-FROM Tokoyo_Olympics.medals
-JOIN Tokoyo_Olympics.olympics ON medals.Team=olympics.Team;
+FROM Tokyo_Olympics.medals
+JOIN Tokyo_Olympics.olympics ON medals.Team=olympics.Team;
 
 # Total Athletes vs Total Medals
 SELECT medals.Team, medals.Total, olympics.TotalAthletes, (medals.Total/olympics.TotalAthletes)*100 AS AthletePerMedal
-FROM Tokoyo_Olympics.medals
-JOIN Tokoyo_Olympics.olympics ON medals.Team=olympics.Team
+FROM Tokyo_Olympics.medals
+JOIN Tokyo_Olympics.olympics ON medals.Team=olympics.Team
 ORDER BY AthletePerMedal DESC
 
 
